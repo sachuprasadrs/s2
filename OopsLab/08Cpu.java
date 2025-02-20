19501cb3b6180363ccc5aacb984e5f16c9d63721
sachuprasadrs <sachuprasadrs@gmail.com>
2025-02-20 15:42:34 +0530

    Update 08Cpu.java

--- OopsLab/08Cpu.java
+++ OopsLab/08Cpu.java
@@ -1,8 +1,22 @@
 import java.util.*;
 public class Cpu{
 	int price;
+	
 	class Processor{
 		int core;
-		string manufacture;
+		String manufacture;
+		
+		static class Ram{
+			int memory;
+			String manufacture;
+		}
+	}
+}
+public class Demo{
+	public static void main(String args[]){
+	{
+		Cpu obj=new Cpu();
+		obj.price=1000;
+		
 	}
 }

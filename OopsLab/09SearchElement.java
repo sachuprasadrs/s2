d3052904a6924218cd3c0c04340a9daddde6762e
sachuprasadrs <sachuprasadrs@gmail.com>
2025-02-27 15:33:19 +0530

    Update 09SearchElement.java

--- OopsLab/09SearchElement.java
+++ OopsLab/09SearchElement.java
@@ -28,3 +28,38 @@ class SearchElement {
 		sc.close();
 	}
 }
+/*
+1. Input the size of the array:
+
+    Prompt the user to input the number of elements n that will be in the array.
+    Read the integer value n from the user.
+
+2. Declare and initialize the array:
+
+    Declare an array arr of strings with size n.
+    Prepare to store the elements of the array entered by the user.
+
+3. Input the array elements:
+
+    Print a message prompting the user to enter n elements.
+    For each index i from 0 to n-1, prompt the user to input a string and store it in arr[i].
+
+4. Input the element to search:
+
+    Print a message asking the user to enter the element they want to search for.
+    Read the search element (key) from the user.
+
+5. Search for the element:
+
+    Initialize a variable position to -1 (indicating the element is not found by default).
+    Loop through the array, checking each element:
+        If the current element equals the search key (using .equals()), update the position to the index i and break out of the loop.
+
+6. Check if the element was found:
+
+    If position is not -1, print a message indicating that the element was found at the index position.
+    If position is still -1, print a message indicating that the element was not found in the array.
+
+7. Close the scanner:
+
+    Close the Scanner object to free up resources after the input operation is complete.*/

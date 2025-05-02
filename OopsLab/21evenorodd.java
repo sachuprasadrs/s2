448e8ce8f8d9e89c064d53b2ad271a67dbaa240d
sachuprasadrs <sachuprasadrs@gmail.com>
2025-05-02 11:26:54 +0530

    Create 21evenorodd.java

---
+++ OopsLab/21evenorodd.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+class EvenException extends Exception {
+	EvenException(String s) {
+		super(s);
+	}
+}
+class OddException extends Exception {
+	OddException(String s) {
+		super(s);
+	}
+}
+class Check {
+	static void evenorodd(int n) throws EvenException, OddException {
+		if (n % 2 == 0) {
+			throw new EvenException("Even number");
+		}
+		else {
+			throw new OddException("Odd number");
+		}
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number");
+		int n = sc.nextInt();
+		try {
+			evenorodd(n);
+		} 
+		catch(Exception e) {
+			System.out.println(e);
+		}
+	}
+}

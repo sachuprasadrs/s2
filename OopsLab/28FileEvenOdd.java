e7587dac6e12014586d29b55f98f86f2c0b3d6c0
sachuprasadrs <sachuprasadrs@gmail.com>
2025-05-19 15:37:05 +0530

    Create 28FileEvenOdd.java

---
+++ OopsLab/28FileEvenOdd.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+public class FileEvenOdd{
+	public static void main(String[] args){
+		try {
+			Scanner sc = new Scanner(System.in);
+			
+		}
+		catch (Exception e){
+			System.out.println("An error occurred: " + e.getMessage());
+		}
+	}
+}

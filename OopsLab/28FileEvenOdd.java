6a0b3c504a9728403721253e7402095691941769
sachuprasadrs <sachuprasadrs@gmail.com>
2025-05-19 15:56:14 +0530

    Update 28FileEvenOdd.java

--- OopsLab/28FileEvenOdd.java
+++ OopsLab/28FileEvenOdd.java
@@ -1,13 +1,35 @@
 import java.io.*;
-import java.util.*;
-public class FileEvenOdd{
-	public static void main(String[] args){
-		try {
-			Scanner sc = new Scanner(System.in);
-			
-		}
-		catch (Exception e){
-			System.out.println("An error occurred: " + e.getMessage());
-		}
-	}
+class FilEvenOdd {
+public static void main(String args[]) throws IOException {
+FileInputStream f = new FileInputStream("number.txt");
+FileOutputStream f2 = new FileOutputStream("odd.txt");
+FileOutputStream f3 = new FileOutputStream("even.txt"); 
+int c;
+while ((c = f.read()) != -1) {
+if (Character.isDigit(c)) {
+int digit = c - '0';
+if (digit % 2 == 0) {
+f3.write(c);
+} else {
+f2.write(c);
+}
+}
+System.out.print((char) c);
+}
+f.close();
+f2.close();
+f3.close();
+System.out.println("\n\nContents of odd.txt:");
+try (FileInputStream oddFile = new FileInputStream("odd.txt")) {
+while ((c = oddFile.read()) != -1) {
+System.out.print((char) c);
+}
+}
+System.out.println("\nContents of even.txt:");
+try (FileInputStream evenFile = new FileInputStream("even.txt")) {
+while ((c = evenFile.read()) != -1) {
+System.out.print((char) c);
+}
+}
+}
 }

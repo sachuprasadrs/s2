1b7a9eee0b1e2a7c9ca64c92dbf260a657a38cf8
sachuprasadrs <sachuprasadrs@gmail.com>
2025-06-12 23:18:33 +0530

    Update 17prime.sh

--- CNLab/17prime.sh
+++ CNLab/17prime.sh
@@ -1,25 +1,14 @@
 is_prime() {
-  num=$1
-  if [ "$num" -lt 2 ]
-  then
-    return 1
-  fi
-  for ((i = 2; i * i <= num; i++))
-  do
-    if (( num % i == 0 ))
-    then
-      return 1
-    fi
+  [ $1 -le 1 ] && return 1
+  for ((i=2; i*i<=$1; i++)); do
+    [ $(( $1 % i )) -eq 0 ] && return 1
   done
   return 0
 }
+
 read -p "Enter lower limit: " l
 read -p "Enter upper limit: " u
-echo "Prime numbers between $l and $u:"
-for ((n = l; n <= u; n++))
-do
-  if is_prime "$n"
-  then
-    echo "$n"
-  fi
+echo "Primes between $l and $u:"
+for ((n=l; n<=u; n++)); do
+  is_prime $n && echo $n
 done
